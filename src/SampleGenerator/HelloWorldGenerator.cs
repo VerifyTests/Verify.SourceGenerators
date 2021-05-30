c1d0bc7ce510bf0cdfcb891b54d39d963077a0e2
Simon Cropp <simon.cropp@gmail.com>
2021-05-30 19:12:13 +1000

    .

--- src/SampleGenerator/HelloWorldGenerator.cs
+++ src/SampleGenerator/HelloWorldGenerator.cs
@@ -8,17 +8,16 @@ public class HelloWorldGenerator :
 {
     public void Execute(GeneratorExecutionContext context)
     {
-        var sourceBuilder = new StringBuilder(@"
+        var source = @"
 using System;
 public static class HelloWorld
 {
     public static void SayHello()
     {
         Console.WriteLine(""Hello from generated code!"");
-        Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
     }
-}");
-        context.AddSource("helloWorldGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+}";
+        context.AddSource("helloWorldGenerator", SourceText.From(source, Encoding.UTF8));
     }
 
     public void Initialize(GeneratorInitializationContext context)
--- src/Tests/Samples.SimpleGeneratorTest.01.verified.cs
+++ src/Tests/Samples.SimpleGeneratorTest.01.verified.cs
@@ -5,6 +5,5 @@ public static class HelloWorld
     public static void SayHello()
     {
         Console.WriteLine("Hello from generated code!");
-        Console.WriteLine("The following syntax trees existed in the compilation that created this program:");
     }
 }
