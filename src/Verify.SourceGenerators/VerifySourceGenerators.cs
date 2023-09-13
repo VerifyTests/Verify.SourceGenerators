5c065d7a9798a32a85c3234f82f263fc048b0336
Simon Cropp <simon.cropp@gmail.com>
2023-09-13 23:04:58 +1000

    Update VerifySourceGenerators.cs

--- src/Verify.SourceGenerators/VerifySourceGenerators.cs
+++ src/Verify.SourceGenerators/VerifySourceGenerators.cs
@@ -6,10 +6,6 @@ public static class VerifySourceGenerators
 {
     public static bool Initialized { get; private set; }
 
-    [Obsolete("Use Initialize()")]
-    public static void Enable() =>
-        Initialize();
-
     public static void Initialize()
     {
         if (Initialized)
