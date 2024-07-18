b78980e7e1a08c2eb5b6cd33c3516f5371f63476
Timothy Makkison <timmakkison@gmail.com>
2024-07-18 13:57:26 +0100

    feat: added `IgnoreGeneratedResultInstance`

---
+++ src/Tests/IgnoreTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+public class IgnoreTest
+{
+    #region IgnoreFile
+    [Fact]
+    public Task IgnoreFile()
+    {
+        var driver = GeneratorDriver();
+
+        return Verify(driver)
+            .IgnoreGeneratedResultInstance(x => x.HintName.Contains("helper"))
+            .IgnoreGeneratedResultInstance(x => x.SourceText
+                .ToString()
+                .Contains("static void SayHello()"));
+    }
+    #endregion
+
+    [Fact]
+    public Task SettingsIgnoreFile()
+    {
+        var driver = GeneratorDriver();
+        var settings = new VerifySettings();
+        settings.IgnoreGeneratedResultInstance(x => x.HintName.Contains("helper"));
+        settings.IgnoreGeneratedResultInstance(x => x.SourceText
+            .ToString()
+            .Contains("static void SayHello()"));
+
+        return Verify(driver, settings);
+    }
+
+    static GeneratorDriver GeneratorDriver()
+    {
+        var compilation = CSharpCompilation.Create("name");
+        var generator = new HelloWorldGenerator();
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        return driver.RunGenerators(compilation);
+    }
+}
---
+++ src/Verify.SourceGenerators/VerifySourceGeneratorsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace VerifyTests;
+
+public static class VerifySourceGeneratorsExtensions
+{
+    internal const string IgnoreContextName = $"{nameof(VerifySourceGenerators)}.{nameof(IgnoreGeneratedResultInstance)}";
+
+    public static SettingsTask IgnoreGeneratedResultInstance(this SettingsTask settingsTask, Func<GeneratedSourceResult, bool> shouldIgnore)
+    {
+        settingsTask.CurrentSettings.IgnoreGeneratedResultInstance(shouldIgnore);
+        return settingsTask;
+    }
+
+    public static void IgnoreGeneratedResultInstance(this VerifySettings verifySettings, Func<GeneratedSourceResult, bool> shouldIgnore)
+    {
+        if (!verifySettings.Context.TryGetValue(IgnoreContextName, out var value))
+        {
+            value = new List<Func<GeneratedSourceResult, bool>>();
+            verifySettings.Context.Add(IgnoreContextName, value);
+        }
+
+        if (value is not List<Func<GeneratedSourceResult, bool>> ignoreList)
+        {
+            throw new($"Unexpected value in {nameof(verifySettings.Context)}, type is not {nameof(List<Func<GeneratedSourceResult, bool>>)}");
+        }
+
+        ignoreList.Add(shouldIgnore);
+    }
+}
