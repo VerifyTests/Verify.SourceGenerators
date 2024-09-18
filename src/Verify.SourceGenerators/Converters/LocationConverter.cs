6d92dd9e4193f4b35eefa7f712dc83b4129b5b50
Simon Cropp <simon.cropp@gmail.com>
2024-09-18 21:23:30 +1000

    Update LocationConverter.cs

--- src/Verify.SourceGenerators/Converters/LocationConverter.cs
+++ src/Verify.SourceGenerators/Converters/LocationConverter.cs
@@ -7,31 +7,41 @@
     {
         var lineSpan = value.GetMappedLineSpan();
 
-        // Pretty-print the error with the source code snippet.
-        if (value.SourceTree is { } source)
-        {
-            var comment = new StringBuilder().AppendLine();
-            var lines = source.GetText().Lines;
-            var startLine = Math.Max(lineSpan.StartLinePosition.Line - contextLines, 0);
-            var endLine = Math.Min(lineSpan.EndLinePosition.Line + contextLines, lines.Count - 1);
-            for (var lineIdx = startLine; lineIdx <= endLine; lineIdx++)
-            {
-                var line = lines[lineIdx];
-                // print the source line
-                comment.AppendLine(line.ToString());
-                // print squiggly line highlighting the location
-                if (line.Span.Intersection(value.SourceSpan) is { } intersection)
-                {
-                    comment
-                        .Append(' ', intersection.Start - line.Start)
-                        .Append('^', intersection.Length)
-                        .AppendLine();
-                }
-            }
-            writer.WriteComment(comment.ToString());
-            writer.WriteWhitespace("\n");
-        }
+        WriteSourceTree(writer, value, lineSpan);
 
         writer.Serialize(lineSpan);
+    }
+
+    static void WriteSourceTree(VerifyJsonWriter writer, Location value, FileLinePositionSpan lineSpan)
+    {
+        // Pretty-print the error with the source code snippet.
+        if (value.SourceTree is not { } source)
+        {
+            return;
+        }
+
+        var comment = new StringBuilder();
+        comment.AppendLine();
+        var lines = source.GetText().Lines;
+        var startLine = Math.Max(lineSpan.StartLinePosition.Line - contextLines, 0);
+        var endLine = Math.Min(lineSpan.EndLinePosition.Line + contextLines, lines.Count - 1);
+        for (var index = startLine; index <= endLine; index++)
+        {
+            var line = lines[index];
+            // print the source line
+            comment.AppendLine(line.ToString());
+            // print squiggly line highlighting the location
+            if (line.Span.Intersection(value.SourceSpan) is not { } intersection)
+            {
+                continue;
+            }
+
+            comment.Append(' ', intersection.Start - line.Start);
+            comment.Append('^', intersection.Length);
+            comment.AppendLine();
+        }
+
+        writer.WriteComment(comment.ToString());
+        writer.WriteWhitespace("\n");
     }
 }
